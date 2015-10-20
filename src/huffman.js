8b9f40f6d9af2f4cee1b839752702b7aec8d997e
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-10-19 21:13:53 -0700

    workin on huffman tree

--- src/huffman.js
+++ src/huffman.js
@@ -1,13 +1,56 @@
 // // Given a Huffman tree and a string, encode that string into a new string
 // // consisting only of 1s and 0s, using the code given by the tree.
 var encodeString = function(input, huffmanTree) {
-  return "";
+  var result = '';
+  var inputArr = input.split('');
+  
+  var recurse = function(char, huffmanTree) {
+    if(huffmanTree.val.length === 1 && char === huffmanTree.val[0]) {
+      return;
+    } else if(huffmanTree.left.val.indexOf(char) > -1) {
+      result += '0';
+      recurse(char, huffmanTree.left);
+    } else if(huffmanTree.right.val.indexOf(char) > -1) {
+      result += '1';
+      recurse(char, huffmanTree.right);
+    } else {
+      return "error";
+    }
+  };
+
+  for(var i = 0; i < inputArr.length; i++) {
+    recurse(inputArr[i], huffmanTree);
+  }
+
+  return result;
+
 };
 
 // // Given a Huffman tree and a string of 1s and 0s, decode that string into
 // // a new, human-readable string, using the code given by the tree.
 var decodeString = function(input, huffmanTree) {
-  return "";
+  var result = '';
+  var inputArr = input.split('');
+
+  var recurse = function(input, tree) {
+    if(tree.val.length === 1) {
+      result += tree.val;
+      input.shift();
+      recurse(input, huffmanTree);
+    } else if(input[0] === '0') {
+      input.shift();
+      recurse(input, tree.left);
+    } else if(input[0] === '1') {
+      input.shift();
+      recurse(input, tree.right);
+    } else {
+      return "error";
+    }
+  }
+  // debugger;
+  recurse(inputArr, huffmanTree);
+  return result;
+
 };
 
 // Given a corpus of text, return a Huffman tree that represents the
